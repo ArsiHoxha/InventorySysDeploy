97bf79026a521988f6ac11c2da778b7efb449537
Arsi Hoxha <arsihoxha23@gmail.com>
2024-08-19 01:00:17 -0400

    7

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'https://inventorysysdeploy-1-front.onrender.com/',
+  origin: 'https://inventorysysdeploy-1-front.onrender.com',
   credentials: true
 }));
 
